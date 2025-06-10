264da7d7f6dccbe344afcd74587b7c5ba8cb9b8b
Purbohadi Utomo <purbohadi.utomo@gmail.com>
2025-06-10 08:42:42 +0700

    Refactor MainLayout test imports for improved clarity by removing unnecessary fireEvent import.

--- __test__/src/components/layout/MainLayout.test.tsx
+++ __test__/src/components/layout/MainLayout.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, screen, fireEvent } from '../../test-utils';
+import { render, screen } from '../../test-utils';
 import MainLayout from '../../../../src/components/layout/MainLayout';
 
 describe('MainLayout', () => {
